efbd7e9769657849c13f054121e3d4d2d8182d7a
blossommohana97 <30368714+blossommohana97@users.noreply.github.com>
2017-07-22 17:01:45 +0530

    Create mohana.c

---
+++ mohana.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter any num:");
+scanf("%d",&num);
+if(num>0)
+{
+printf("postive num");
+}
+else if(num<0)
+{
+printf("negative num");
+}
+else
+{
+printf("zero");
+}}
